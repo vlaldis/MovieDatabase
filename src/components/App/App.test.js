9898507f0503b8e72e610853a4414ad9d19bcfd3
Vladimir Varga <v.varga@azet.sk>
2023-02-23 09:29:32 +0100

    Add base layout

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
+test('renders Appbar', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
+  const linkElement = screen.getByText(/Movie Store/i)
   expect(linkElement).toBeInTheDocument()
 })
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,8 @@
+
+import { Stack } from '@mui/material'
+import { AppHeader } from './AppHeader/AppHeader'
+
+export const Layout = (children) =>
+  <Stack>
+    <AppHeader />
+  </Stack>
